aa060d2a6c5f7febb0065502871c0d9788011cd3
armandozabala <armandozabala@gmail.com>
2019-07-14 21:39:46 -0500

    update material functions

--- src/app/components/materiaprima/materiaprima.component.ts
+++ src/app/components/materiaprima/materiaprima.component.ts
@@ -22,6 +22,10 @@ export class MateriaprimaComponent implements OnInit {
 
   materia: any;
 
+  materialText:any;
+
+  ids:any;
+
   constructor(private dataService: FirebaseService) {
 
     this.forma = new FormGroup({
@@ -30,8 +34,7 @@ export class MateriaprimaComponent implements OnInit {
        'costs':  new FormControl('',Validators.required),
        'unity':  new FormControl('',Validators.required),
        'code':  new FormControl(''),
-       'cost_unity':  new FormControl(''),
-       'id':  new FormControl('')
+       'cost_unity':  new FormControl('')
     });
 
 
@@ -79,14 +82,14 @@ export class MateriaprimaComponent implements OnInit {
         this.materia.id = id;
 
 
+
         this.forma = new FormGroup({
           'description' : new FormControl(this.materia.description, Validators.required),
           'quantity': new FormControl(this.materia.quantity,Validators.required),
           'costs':  new FormControl(this.materia.costs,Validators.required),
           'unity':  new FormControl(this.materia.unity,Validators.required),
-          'code':  new FormControl(this.materia.code),
-          'cost_unity':  new FormControl(this.materia.cost_unity),
-          'id': new FormControl(this.materia.id),
+          'code':  new FormControl(this.materia.code.toUpperCase()),
+          'cost_unity':  new FormControl(this.materia.cost_unity)
        });
 
        this.forma.get('quantity')
@@ -98,7 +101,7 @@ export class MateriaprimaComponent implements OnInit {
         this.forma.get('costs')
         .valueChanges
         .subscribe(value => this.forma.get('cost_unity')
-          .setValue(isNaN(value) ? 0 : value =  this.forma.get('quantity').value / this.forma.get('costs').value )
+          .setValue(isNaN(value) ? 0 : value = this.forma.get('quantity').value / this.forma.get('costs').value )
         );
 
     });
@@ -106,8 +109,9 @@ export class MateriaprimaComponent implements OnInit {
 
   editMaterial(){
 
-    console.log('EDIT FormGroup', this.forma.value);
-    const id = this.forma.get('id').value;
+    this.forma.value.code = this.forma.value.code.toUpperCase();
+    console.log(this.materia.id);
+    const id = this.materia.id;
 
     this.dataService.updateMaterial(id, this.forma.value).then(function(res){
          $('#editMaterialModal').modal('toggle');
@@ -119,9 +123,8 @@ export class MateriaprimaComponent implements OnInit {
 
   addMaterial() {
 
+    this.forma.value.code = this.forma.value.code.toUpperCase();
     console.log('FormGroup', this.forma.value);
-
-    console.log('FORMULARIO');
 
     this.dataService.addMaterial(this.forma.value).then(function(res){
         console.log(res.id);
@@ -132,9 +135,18 @@ export class MateriaprimaComponent implements OnInit {
 
   }
 
+  confirmDelete(ids: string, materialText: string){
+    console.log(ids);
+    this.ids = ids;
+    this.materialText = materialText;
+
+    $('#confirmMaterialModal').modal('toggle');
+  }
+
   deleteMaterial(id: string) {
      this.dataService.deleteMaterial(id).then(function(res){
         //console.log(res);
+        $('#confirmMaterialModal').modal('toggle');
      }).catch(function(err){
         console.log(err);
      });
